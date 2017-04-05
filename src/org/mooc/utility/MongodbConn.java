c26a543c7589f7ab514779e92bd8647a45411e04
wuke1993 <2297184250@qq.com>
2017-04-05 11:39:08 +0800

    Add function to connect MongoDb with password.

--- src/org/mooc/utility/MongodbConn.java
+++ src/org/mooc/utility/MongodbConn.java
@@ -1,32 +1,53 @@
 package org.mooc.utility;
 
+import java.util.Arrays;
+
 import org.bson.Document;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-/**
- * @author: wuke 
- * @date  : 2016年12月4日 下午8:27:52
- * Title  : MongoConn
- * Description : 
- */
 public class MongodbConn {
-	public static MongoCollection<Document> getMongoCollection(String databaseName, String mongoCollectionName) {
+    public static MongoCollection<Document> getMongoCollection(String databaseName, String mongodbCollectionName){
+    	ServerAddress ip = new ServerAddress("localhost",27017);
+    	String userName = "root";
+    	String password = "root";
+    	MongoCredential credential = MongoCredential.createCredential(userName, databaseName, password.toCharArray());
+        
+    	try{
+			MongoClient mongoClient = new MongoClient(ip, Arrays.asList(credential));
+			
+            MongoDatabase mongoDatabase = mongoClient.getDatabase(databaseName);
+            // System.out.println("Successfully connect to MongoDB" + databaseName + "!");
+            
+            MongoCollection<Document> mongoCollection = mongoDatabase.getCollection(mongodbCollectionName);
+            System.out.println("Successfully get collection " + mongodbCollectionName + "!");
+            return mongoCollection;
+
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    // no password
+	public static MongoCollection<Document> getMongoCollectionNoAuthentication(String databaseName, 
+			String mongodbCollectionName) {
 		try {
 			MongoClient mongoClient = new MongoClient("localhost", 27017);
 			
 			MongoDatabase mongoDatabase = mongoClient.getDatabase(databaseName);
-			//System.out.println("Successfully connect to mongodb " + databaseName);
+			// System.out.println("Successfully connect to MongoDB " + databaseName + "!");
 			
-			MongoCollection<Document> mongoCollection = mongoDatabase.getCollection(mongoCollectionName);
-			System.out.println("Successfully get collection " + mongoCollectionName + "!");
+			MongoCollection<Document> mongoCollection = mongoDatabase.getCollection(mongodbCollectionName);
+			System.out.println("Successfully get collection " + mongodbCollectionName + "!");
 			
 			return mongoCollection;
 		} catch (Exception e) {
 			e.printStackTrace();
-			System.out.println("Get collection" + mongoCollectionName + " failed!");
 			return null;
 		}
 	}
