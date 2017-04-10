7cddf6add33ba28ffb5e41cd8de7cfe1915ddcba
wuke1993 <2297184250@qq.com>
2017-04-10 17:20:33 +0800

    Add MongoDBSplit.

---
+++ src/org/mooc/utility/MongoDBSplit.java
@@ -0,0 +1,44 @@
+package org.mooc.utility;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+
+/**
+* @author : wuke
+* @date   : 2017年4月10日下午4:01:53
+* Title   : TestMongoDBSplit
+* Description : 
+*/
+public class MongoDBSplit {
+
+	public static void main(String[] args) {
+		List<Document> docs = MongoDBSplit.page(7, 10);
+		for(Document doc : docs)
+			System.out.println(doc);
+	}
+
+	static List<Document> page(int page, int pageSize) {
+		List<Document> docs = new ArrayList<Document>();
+		MongoCollection<Document> collection = MongoDBConn.getMongoCollection("mooc", "frequentCourses");
+		
+		BasicDBObject sort = new BasicDBObject();
+		sort.put("count", -1);
+		
+		MongoCursor<Document> cursor = 
+				collection.find().sort(sort).skip((page -1) * pageSize).limit(pageSize).iterator();
+		
+		while(cursor.hasNext()) {
+			Document doc = cursor.next();
+			docs.add(doc);
+		}
+		
+		return docs;
+	}
+}
