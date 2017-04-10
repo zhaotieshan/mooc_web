1edaacb7f1a4d86dcd801821c193c970d5c8a065
wuke1993 <2297184250@qq.com>
2017-04-10 10:03:45 +0800

    Delete function readFrequentCourses, add filter rules.

--- src/org/mooc/recommend/frequentPattern/GenFrequentRec.java
+++ src/org/mooc/recommend/frequentPattern/GenFrequentRec.java
@@ -8,64 +8,74 @@ import org.bson.Document;
 import org.mooc.utility.MongodbConn;
 
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
 import static com.mongodb.client.model.Filters.*;
 
 /**
 * @author : wuke
 * @date   : 2016年12月22日下午5:02:56
-* Title   : GenerateFrequentRec
-* Description : 
+* Title   : GenFrequentRec
+* Description : Generate frequent courses pairs for every user. 
+*   Then store the recommendations into MongoDB mooc.frequentRec. 
 */
 public class GenFrequentRec {
-
-	private final static String MONGODB_NAME = "mooc";
 	
 	public static void main(String[] args) {
-		Map<String, ArrayList<String>> userCoursesMap = GenFrequentRec.readUserCourses();
-		
-		long startTime = System.currentTimeMillis();
-		
-		System.out.println("Start generate frequent recommendations for users!");
-		GenFrequentRec.generateFrequentRec(userCoursesMap);
-		
-		long cost = System.currentTimeMillis() - startTime;
-		System.out.println("Cost " + cost + " milliseconds!");
+		GenFrequentRec.generateFrequentRec(GenFrequentRec.readUserCourses());
 	}
 	
 	/**
-	 * frequent recommendation = user_learned_courses + frequent_two_courses 
+	 * user_learned_courses & frequent_two_courses -> Frequent recommendation.
 	 * @param userCoursesMap
-	 * @param frequentTwoItemsetMap
+	 * An example of the recommendation, 
+	 * {
+	 *     "_id" : ObjectId("58eae309d1ad2112f04b1e8b"),
+	 *     "userId" : "cdb848ca-d702-4ae1-83f6-1f1e67c800ce",
+	 *     "course1" : "0d7973e0-67b1-4a0a-bbd4-fd2f0d6be887",
+	 *     "course1Name" : "CAD制图",
+	 *     "course2" : "daf84e78-9fd6-4e17-be58-f71c38cee288",
+	 *     "course2Name" : "计算机安全",
+	 *     "count" : NumberInt("18")
+	 * }
 	 */
 	static void generateFrequentRec(Map<String, ArrayList<String>> userCoursesMap) {
-		String frequentRec_collection_name = "frequentRec";
 		MongoCollection<Document> frequentRec_collection = 
-				MongodbConn.getMongoCollection(MONGODB_NAME, frequentRec_collection_name);
+				MongodbConn.getMongoCollection("mooc", "frequentRec");
+		frequentRec_collection.drop(); // delete the old data
+		frequentRec_collection = MongodbConn.getMongoCollection("mooc", "frequentRec");
 		
-		String frequentCourses_collection_name = "frequentCourses";
 		MongoCollection<Document> frequentCourses_collection = 
-				MongodbConn.getMongoCollection(MONGODB_NAME, frequentCourses_collection_name);
+				MongodbConn.getMongoCollection("mooc", "frequentCourses");
 		
-		// iterate every record(every user) in userCoursesMap
+		// Iterate every record in userCoursesMap
 		String userId = "";
 		ArrayList<String> coursesSet = null;
 		for(Map.Entry<String, ArrayList<String>> entry : userCoursesMap.entrySet()) {
 			userId = entry.getKey();
 			coursesSet = entry.getValue();
 			
-			// iterate every course the user has learned, search the frequentCourses
-			String course2 = "";
+			// Iterate through the courses user has learned, search the frequentCourses
+			String course2 = null;
+			String course1Name = null;
+			String course2Name = null;
 			int count = 0;
 			for(String course : coursesSet) {
-				for(Document doc : frequentCourses_collection.find(eq("course1",course))) {
+				for(Document doc : frequentCourses_collection.find(eq("course1",course))) {					
 					course2 = doc.getString("course2");
+					
+					// Judge if the course2 has already been learned
+					if(coursesSet.contains(course2))
+						continue;
+					
+					course1Name = doc.getString("course1Name");
+					course2Name = doc.getString("course2Name");
 					count = doc.getInteger("count");
 					
-					Document rec = new Document(); // "userId", "course1", "course2", "count"
+					Document rec = new Document();
 					rec.append("userId", userId);
 					rec.append("course1", course);
+					rec.append("course1Name", course1Name);
 					rec.append("course2", course2);
+					rec.append("course2Name", course2Name);
 					rec.append("count", count);
 					
 					frequentRec_collection.insertOne(rec);
@@ -75,7 +85,7 @@ public class GenFrequentRec {
 	}
 	
 	/**
-	 * read records of user_learned_courses from mooc.userCourses
+	 * Read records of user_learned_courses from MongoDB mooc.userCourses.
 	 * {
 	 *     "_id" : ObjectId("5857a528d51d2e46cc5ea75f"),
 	 *     "userId" : "02e2fbf7-bda6-44ca-b8c9-59efeb534472",
@@ -86,11 +96,11 @@ public class GenFrequentRec {
      * }
 	 * @return userCoursesMap
 	 */
+	@SuppressWarnings("unchecked")
 	static Map<String, ArrayList<String>> readUserCourses() {
 		Map<String, ArrayList<String>> userCoursesMap = new HashMap<String, ArrayList<String>>();
 		
-		String mongodb_collection = "userCourses";
-		MongoCollection<Document> collection = MongodbConn.getMongoCollection(MONGODB_NAME, mongodb_collection);
+		MongoCollection<Document> collection = MongodbConn.getMongoCollection("mooc", "userCourses");
 		
 		String userId = "";
 		ArrayList<String> coursesSet = null; 
@@ -103,37 +113,4 @@ public class GenFrequentRec {
 		
 		return userCoursesMap;
 	}
-	
-	/**
-	 * Not use!
-	 * read the records of frequent_two_courses from mooc.frequentCourses
-	 * {
-	 *     "_id" : ObjectId("585b3403d51d2e0994796bd2"),
-	 *     "course1" : "050fb0b8-bffe-4b28-994d-2190debef53b",
-	 *     "course2" : "0d7973e0-67b1-4a0a-bbd4-fd2f0d6be887",
-	 *     "count" : NumberInt("13")
-     * }    
-	 * @return frequentTwoItemsetMap
-	 */
-	/*static Map<String, Integer> readFrequentCourses() {
-		Map<String, Integer> frequentTwoItemsetMap = new HashMap<String, Integer>();
-		
-		String mongodb_collection = "frequentCourses";
-		MongoCollection<Document> collection = MongodbConn.getMongoCollection(MONGODB_NAME, mongodb_collection);
-		
-		String course1 = "";
-		String course2 = "";
-		int count = 0; 
-		String temp = "";
-		for(Document doc : collection.find()) {
-			course1 = doc.getString("course1");
-			course2 = doc.getString("course2");
-			count = doc.getInteger("count");
-			temp = course1 + "," + course2;
-			
-			frequentTwoItemsetMap.put(temp, count);
-		}
-		
-		return frequentTwoItemsetMap;
-	}*/
 }
