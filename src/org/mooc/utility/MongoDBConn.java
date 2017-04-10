8316eea2e38382029451d4c5ee2045631b91baeb
wuke1993 <2297184250@qq.com>
2017-04-10 22:09:01 +0800

    Singleton Pattern.

--- src/org/mooc/utility/MongoDBConn.java
+++ src/org/mooc/utility/MongoDBConn.java
@@ -11,44 +11,56 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 public class MongoDBConn {
-    public static MongoCollection<Document> getMongoCollection(String databaseName, String mongodbCollectionName){
+	private static MongoClient MONGOCLIENT = null;
+	
+	/**
+	 * Return MongoCollection
+	 * @param databaseName
+	 * @param mongodbCollectionName
+	 * @return mongoCollection
+	 */
+    public static MongoCollection<Document> getMongoCollection(String databaseName, String mongodbCollectionName) {
+    	MongoDatabase mongoDatabase = null;
+    	MongoCollection<Document> mongoCollection = null;
+    	
+    	// Double Check Lock
+    	if(MONGOCLIENT == null) {
+    		synchronized(MongoDBConn.class) {
+    		    if(MONGOCLIENT == null)
+    		    	MongoDBConn.initMongoClient();
+    	    }
+    	}
+        mongoDatabase = MONGOCLIENT.getDatabase(databaseName);
+        mongoCollection = mongoDatabase.getCollection(mongodbCollectionName);
+        
+        System.out.println("Successfully get collection " + mongodbCollectionName + "!");
+        return mongoCollection;
+    }
+    
+    /**
+     * Get MongoClient with authentication
+     */
+    private static void initMongoClient() {
     	ServerAddress ip = new ServerAddress("localhost",27017);
     	String userName = "root";
     	String password = "root";
-    	MongoCredential credential = MongoCredential.createCredential(userName, databaseName, password.toCharArray());
-        
-    	try{
-			MongoClient mongoClient = new MongoClient(ip, Arrays.asList(credential));
-			
-            MongoDatabase mongoDatabase = mongoClient.getDatabase(databaseName);
-            // System.out.println("Successfully connect to MongoDB" + databaseName + "!");
-            
-            MongoCollection<Document> mongoCollection = mongoDatabase.getCollection(mongodbCollectionName);
-            System.out.println("Successfully get collection " + mongodbCollectionName + "!");
-            return mongoCollection;
-
-        }catch(Exception e){
-            e.printStackTrace();
-            return null;
-        }
+    	String databaseName = "mooc";
+    	try {
+    	    MongoCredential credential = MongoCredential.createCredential(userName, databaseName, password.toCharArray());
+    	    MONGOCLIENT = new MongoClient(ip, Arrays.asList(credential));
+    	} catch(Exception e) {
+    		e.printStackTrace();
+    	}  	
     }
     
-    // no password
-	public static MongoCollection<Document> getMongoCollectionNoAuthentication(String databaseName, 
-			String mongodbCollectionName) {
+    /**
+     * Get MongoClient with no authentication
+     */
+	public static void initMongoClientNoAuthentication() {
 		try {
-			MongoClient mongoClient = new MongoClient("localhost", 27017);
-			
-			MongoDatabase mongoDatabase = mongoClient.getDatabase(databaseName);
-			// System.out.println("Successfully connect to MongoDB " + databaseName + "!");
-			
-			MongoCollection<Document> mongoCollection = mongoDatabase.getCollection(mongodbCollectionName);
-			System.out.println("Successfully get collection " + mongodbCollectionName + "!");
-			
-			return mongoCollection;
-		} catch (Exception e) {
+			MONGOCLIENT = new MongoClient("localhost", 27017);
+		} catch(Exception e) {
 			e.printStackTrace();
-			return null;
 		}
 	}
 }
