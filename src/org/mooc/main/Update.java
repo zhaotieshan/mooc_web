4b95ad852de14d6347a540c72c7ba30c81c024bc
wuke1993 <2297184250@qq.com>
2017-04-14 16:47:37 +0800

    Remove updating of the courses and users.

--- src/org/mooc/main/Update.java
+++ src/org/mooc/main/Update.java
@@ -3,10 +3,8 @@ package org.mooc.main;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.mooc.processing.courses.CrawlCourses;
 import org.mooc.processing.logs.GenUserLearnedCourses;
 import org.mooc.processing.logs.ProcessLogs;
-import org.mooc.processing.users.CrawlUsers;
 import org.mooc.recommend.frequentPattern.GenAprioriDataset;
 import org.mooc.recommend.frequentPattern.GenFrequentCourses;
 import org.mooc.recommend.frequentPattern.GenFrequentRec;
@@ -20,29 +18,34 @@ import org.mooc.recommend.frequentPattern.GenFrequentRec;
 class Update implements Runnable {
 	public void run() {
 		try {
-			System.out.println("***************** Upate start *****************");
+			System.out.println("***************** Update start *****************");
 			long start_time=System.currentTimeMillis();
 			
 			// Current date
-			/*Date date = new Date();
+			Date date = new Date();
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd"); 
 			String dateString = sdf.format(date);
 			
-			// One day's increment
-			CrawlCourses.crawlCourses();
-			CrawlUsers.crawlUsers();
+			/* 
+			 * One day's increment, 
+			 * (logs -> the user-learned-courses -> frequent-courses-pairs -> frequent course recommendations).
+			 * Notice : 
+			 * 1. course is update in Class GenFrequentCourses just when it need to be updated; 
+			 * 2. users don't need to update yet.
+			 */
 			ProcessLogs.oneDayIncrease(dateString);
 			
 			GenUserLearnedCourses.oneDayIncrease(dateString);
 			
 			GenAprioriDataset.generateRecords();
 			GenFrequentCourses.genFrequentCourses();
-			GenFrequentRec.generateFrequentRec();*/
+			GenFrequentRec.generateFrequentRec();
 			
 			long end_time=System.currentTimeMillis();
-			System.out.println("***************** Upate finish *****************");
+			System.out.println("***************** Update finished *****************");
 			long cost = (end_time-start_time) / 1000;
 			System.out.println("***************** Cost " + cost + "s *****************");
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
